6051ed8616cd3f8c3abf54ffb7b90767310fc793
VINAYREDDY4466 <118588257+VINAYREDDY4466@users.noreply.github.com>
2024-12-13 12:21:22 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 import {BrowserRouter} from 'react-router-dom'
-import { StoreContextProvider } from './Context/StoreContext.jsx';
+import { StoreContextProvider } from './context/StoreContext.jsx';
 
 
 createRoot(document.getElementById('root')).render(
